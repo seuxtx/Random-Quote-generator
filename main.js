39ec4f58ae58e0159b8231b1e54aae11a6a38e18
Brandon Salter <bsalter5444@gmail.com>
2024-04-01 22:19:49 -0400

    change quotes

--- main.js
+++ main.js
@@ -5,12 +5,24 @@ let quote = [
     'You Like backshots?',
     'I love thick goblin girls - Kenny Solomon',
     'Hellhounds are the best monster girls, do not question it',
-    'Stop it ron, stop',
+    'I got eyes on an enemy tank just north of your position',
     'Holy shit, I forgot my socks',
-    '*insert german march*',
+    '*insert a quote*',
     'My friends call my coach',
-    'I would fuck that plant',
-    'San Diego is No. Wan!'
+    'Thank you dragon balls',
+    'San Diego is No. Wan!',
+    'I got messages from the stars',
+    'Music Sounds better with you',
+    'Bring it around town',
+    'The things I do for love',
+    'Stop looking over your shoulder',
+    'Oh I know what the ladies like',
+    'I need a weapon',
+    'Get me pictures of spiderman',
+    'Overtime',
+    'If a bunch of animals are in one location then its a zoo, Unless its a farm.',
+    'Never give up when you are just about done',
+    "I'll Be back!"
 ];
 btn.addEventListener('click',function(){
     var randomQuote = quote[Math.floor(Math.random() * quote.length)]
